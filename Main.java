a27cffbd6421ae8bd944e7deeed2ad0aacecbd30
Lovely Chakraborty <lovelychakraborty500@gmail.com>
2020-06-26 18:06:19 +0530

    MAIN.java file uploaded

---
+++ Main.java
@@ -0,0 +1,164 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+interface Converter{
+   public double convert(double t);
+}
+
+abstract class TemperatureConverter implements Converter{
+  abstract protected boolean isHot(double temparature);
+   public double convert(double t){
+      System.out.println("convert Celsius to Fahrenheit and Celsius to Kelvin respectively");
+      return t;
+   }
+   
+}
+
+abstract class SpeedConverter implements Converter{
+   abstract protected boolean isFast(double Speed);
+   public double convert(double sp){
+      System.out.println("convert Celsius to Fahrenheit and Celsius to Kelvin respectively");
+      return sp;
+   }
+   
+}
+
+class Thermometer extends TemperatureConverter{
+   protected boolean isHot(double temparature){
+            if(temparature>50){
+                System.out.println("Thermometer Temparature is: "+temparature);
+                System.out.println(" Hot Temparature ");
+                return true;
+            }
+             System.out.println("Thermometer Temparature is: "+temparature);
+             System.out.println("Not So Hot ");
+             return false;
+   }
+@Override
+   public double convert(double t){
+      System.out.println("convert kms to mach and kmh to mph respectively.");
+        return t;
+   }
+
+}
+
+class Thermocouple extends TemperatureConverter{
+   protected boolean isHot(double temparature){
+        if(temparature>50){
+                System.out.println("Thermocouple Temparature is: "+temparature);
+                System.out.println(" Hot Temparature ");
+                return true;
+            }
+             System.out.println("Thermocouple Temparature is: "+temparature);
+             System.out.println("Not So Hot ");
+             return false;
+   }
+@Override
+   public double convert(double t){
+      System.out.println("convert kms to mach and kmh to mph respectively.");
+        return t;
+   }
+}
+
+ class PitotTube extends SpeedConverter{
+ protected boolean isFast(double speed){
+            if(speed>100){
+                System.out.println("PitotTube Speed is: "+speed);
+                System.out.println(" Fast Speed ");
+                return true;
+            }
+             System.out.println("PitotTube Speed is: "+speed);
+             System.out.println("Not So Fast ");
+             return false;
+ }
+  @Override
+   public double convert(double sp){
+      System.out.println("convert Celsius to Fahrenheit and Celsius to Kelvin respectively");
+      return sp;
+   }
+
+}
+
+class ShaftLog  extends SpeedConverter{
+  protected boolean isFast(double speed){
+     if(speed>100){
+                System.out.println("ShaftLog Speed is: "+speed);
+                System.out.println(" Fast Speed ");
+                return true;
+            }
+             System.out.println("ShaftLog Speed is: "+speed);
+             System.out.println("Not So Fast ");
+             return false;
+  }
+  @Override
+   public double convert(double sp){
+      System.out.println("convert Celsius to Fahrenheit and Celsius to Kelvin respectively");
+      return sp;
+   }
+   
+}
+
+public class Main{
+   public static void main(String args[]){
+       try{
+ 
+           Converter converter = new Thermometer();
+           Thermometer t1 = (Thermometer) converter;
+           
+           Converter converter1 = new Thermocouple();
+           Thermocouple t2 = (Thermocouple) converter1;
+           
+           Converter converter2 = new PitotTube();
+           PitotTube p1 = (PitotTube) converter2;
+           
+           Converter converter3 = new ShaftLog();
+           ShaftLog st = (ShaftLog) converter3;
+     
+              Scanner s1 = new Scanner(System.in);
+              System.out.println("Enter an input from 1 to 4: ");
+              int num;
+              num= s1.nextInt();
+              System.out.println("Input is: "+num);
+              switch (num) {
+                 case 1:
+                     System.out.println("Input 1");
+                     converter.convert(8.7);
+                     t1.isHot(50.6);
+                      //Thermometer t1;
+                   break;
+                 case 2:
+                     System.out.println("Input 2");
+                     converter1.convert(4.7);
+                     t2.isHot(80.5);
+                     //Thermocouple t2;
+                   break;
+                 case 3:
+                     System.out.println("Input 3");
+                     converter2.convert(7.5);
+                     p1.isFast(40.7);
+                    // PitotTube p1;
+                   break;
+                 case 4:
+                     System.out.println("Input 4");
+                     converter3.convert(6.7);
+                     st.isFast(100.5);
+                    // ShaftLog s1;
+                   break;
+                 default:
+                     System.out.println("Wrong Input");
+                     break;
+               }
+                   
+       }
+       catch (NoSuchElementException exception) {
+            // Output expected NoSuchElementExceptions.
+           // log(exception);
+        } catch (Exception exception) {
+            // Output unexpected Exceptions.
+            //log(exception, false);
+        }
+        
+   }
+}
+      
